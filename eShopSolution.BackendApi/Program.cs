61c5f84ae39236fc0d36210da807ddacade35b1c
vuongthanhtuyen <112794129+vuongthanhtuyen@users.noreply.github.com>
2024-07-17 22:02:49 +0700

    Add fluent user

--- eShopSolution.BackendApi/Program.cs
+++ eShopSolution.BackendApi/Program.cs
@@ -11,6 +11,8 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using eShopSolution.Application.Catolog.Products;
 using Microsoft.OpenApi.Models;
+using FluentValidation.AspNetCore;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 //ConfigureService in Startup
@@ -39,11 +41,13 @@ builder.Services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
 builder.Services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
 builder.Services.AddRazorPages();
 
-
-
-
+//builder.Services.AddTransient<IValidator<LoginRequest>, LoginRequestValidator>();
+//builder.Services.AddTransient<IValidator<RegisterRequest>, RegisterRequestValidator>();
 
 builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation()
+        .AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
 
 builder.Services.AddSwaggerGen(c =>
 {
@@ -111,81 +115,6 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShop Solution", Version = "v1" });
-
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme // headder
-//    {
-//        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
-//                      Enter 'Bearer' [space] and then your token in the text input below.
-//                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-//        Name = "Authorization",
-//        In = ParameterLocation.Header,
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer"
-//    });
-
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement() // yêu cầu truyền header
-//      {
-//        {
-//            new OpenApiSecurityScheme
-//            {
-//            Reference = new OpenApiReference
-//                {
-//                Type = ReferenceType.SecurityScheme,
-//                Id = "Bearer"
-//                },
-//                Scheme = "oauth2",
-//                Name = "Bearer",
-//                In = ParameterLocation.Header,
-//            },
-//            new List<string>()
-//        }
-//    });
-
-//});
-//builder.Services.AddSwaggerGen();
-
-
-Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-Console.WriteLine(issuer + " - " + signingKey);
-//byte[] signingKeyBytes = System.Text.Encoding.UTF8.GetBytes(signingKey);
-
-//builder.Services.AddAuthentication(opt =>
-//{
-//    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//})
-//.AddJwtBearer(options =>
-//{
-//    options.RequireHttpsMetadata = false;
-//    options.SaveToken = true;
-//    options.TokenValidationParameters = new TokenValidationParameters()
-//    {
-//        ValidateIssuer = true,
-//        ValidIssuer = issuer,
-//        ValidateAudience = true,
-//        ValidAudience = issuer,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        ClockSkew = System.TimeSpan.Zero,
-//        IssuerSigningKey = new SymmetricSecurityKey(signingKeyBytes)
-//    };
-//});
-
-
-
-
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//    .AddCookie(options =>
-//    {
-//        options.LoginPath = "/Login/Index";
-//        options.AccessDeniedPath = "/User/Forbidden/";
-//    });
-
-//builder.Services.AddControllersWithViews()
-//         .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>());
 
 builder.Services.AddSession(options =>
 {
